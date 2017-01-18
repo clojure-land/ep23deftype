b555570447af4d0c5992db4414193425aa3793ad
Arne Brasseur <arne@arnebrasseur.net>
2017-01-18 10:20:28 +0100

    init

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject lambdaisland/ep23deftype "0.1.0-SNAPSHOT"
+  :description "Code for Lambda Island Episode 23, deftype and definterface"
+  :url "http://github.com/lambdaisland/ep23definterface"
+  :license {:name "Mozilla Public License 2.0"
+            :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
---
+++ test/ep23deftype/core_test.clj
@@ -0,0 +1,7 @@
+(ns ep23deftype.core-test
+  (:require [clojure.test :refer :all]
+            [ep23deftype.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
